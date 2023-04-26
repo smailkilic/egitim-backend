658481a57942bfe6d7db69d86db6af9eeed87a8d
Ismail Kilic <kilic.2000@hotmail.com>
2023-04-26 15:28:45 +0300

    Metodlar

---
+++ Giris/HazirMetodlar/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+
+namespace HazirMetodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Matematik Hazır Metodlar
+            /* // Pi sayısı
+            Console.WriteLine(Math.PI);
+            // Yuvarlama
+            decimal sayi = 12.1m;
+            float sayi2 = 12.8f;
+            Console.WriteLine(decimal.Ceiling(sayi)); // Verilen sayıyı yukarı yuvarlar.
+            Console.WriteLine(decimal.Round(sayi)); // Verilen sayıyı aşağı yuvarlar.
+            Console.WriteLine(float.Ceiling(sayi2)); // Sonuç 13
+            Console.WriteLine(float.Round(sayi2));
+            // Üs Alma
+            Console.WriteLine(Math.Pow(2,4)); // 2'nin 4.kuvvetini al. Sonuç 16 verecek.
+            // Karekök Alma
+            Console.WriteLine(Math.Sqrt(16)); // Sonuç 4 verecek. */
+
+            #region try-catch
+
+            /* try
+            {
+                Console.WriteLine(sayi2/0);
+                Console.WriteLine(decimal.Parse("ali"));
+            }
+            catch (FormatException ex) // 'ex' adında variable oluşturdum.
+            {
+                Console.WriteLine("Format hatası oluştu!: " + ex.Message);
+            }
+            catch (DivideByZeroException ex) // 0'a bölme hatası.
+            {
+                Console.WriteLine("Sıfıra bölme hatası oluştu!");
+            }
+            catch (Exception ex) // Exception: oluşabilecek tüm hataları içine alır.
+            {
+                Console.WriteLine("Hata oluştu!: " + ex.Message);
+            } */
+            #endregion
+
+            #endregion
+
+            #region String Hazır Metodlar
+            string haber = "Uyuşturucu   ticareti    suçundan     kırmızı bültenle aranan ve 24 yıl kesinleşmiş hapis cezası bulunan suç örgütü lideri Urfi Çetinkaya dün akşam İstanbul'da yakalandı. Çetinkaya, adliyedeki işlemlerinin tamamlanmasının ardından cezaevine gönderilirken operasyona dair yeni ayrıntılar da ortaya çıktı.";
+
+            #region Split, Trim
+            //Trim: string ifadedeki başında ve sonunda olan boşlukları atar.
+            //var temizcumle = haber.Trim();
+            //Console.WriteLine(temizcumle);
+            
+            //// Cumle Sayisi 
+
+            //var cumleler = haber.Split('.');
+            //foreach (var cumle in cumleler)
+            //{
+            //    var yenicumle = cumle.Replace("Urfi", "****")
+            //                       .Replace("Çetinkaya", "*********"); // Urfi Çetinkaya olan yerleri * yapar.
+
+            //    Console.WriteLine(yenicumle);
+            //    var kelimeler = cumle.Split(' ');
+            //    //foreach (var kelime in kelimeler)
+            //    //{
+            //    //    Console.WriteLine(kelime);
+            //    //}
+            //}
+            #endregion
+
+            #region Substring, IndexOf
+            string haber2 = "operasyona dair yeni ayrıntılar da ortaya çıktı.";
+            //Console.WriteLine(haber2.Substring(10)); // 10. index'ten itibaren alır. Öncesini keser.
+            //Console.WriteLine(haber2.Substring(11, 4));// "dair" kelimesini alır.
+            //var index = haber2.IndexOf("dair");
+            //Console.WriteLine(string.Join("", haber2.Reverse())); // Reverse string.Join ile kullanılır.
+            //Console.WriteLine(haber2.Substring(index, 4)); //dair
+            //Console.WriteLine(haber2.ToUpper()); // Cümleyi büyük harflerle yazar.
+            //Console.WriteLine(haber2.ToLowerInvariant()); // Cümleyi küçük harflerle yazar.
+            //Console.WriteLine(haber2.Contains("yeni"));
+
+            #endregion
+
+            #endregion
+
+            #region DateTime
+            // Bulunduğun zamanı verir.
+            /* Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.UtcNow);
+
+            DateTime dogumGunu = new DateTime(2000, 10, 21);
+            DateTime suan = DateTime.Now;
+            TimeSpan zamanFarki = suan - dogumGunu;
+            Console.WriteLine("Toplam Geçen Saat: " +zamanFarki.TotalHours);
+            Console.WriteLine("Toplam Geçen Gün: " +zamanFarki.TotalDays); */
+
+            #endregion
+        }
+    }
+}
---
+++ Giris/Metodlar/Program.cs
@@ -0,0 +1,59 @@
+﻿using MyLibrary;
+namespace Metodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int toplam = 0;
+            toplam = Topla(3, 5);
+            Console.WriteLine($"Toplam: {toplam}");
+            decimal sonuc = Bol(5, 3);
+            Console.WriteLine($"Bölme: {sonuc}"); */
+
+            Console.WriteLine(MyLib.Topla(3, 5));
+            Console.WriteLine(MyLib.Cikar(10, 5));
+        }
+
+        /* // Toplama Metodu 2 adet integer parametre alıp geriye integer tipinden bir sonuç döner.
+        static int Topla(int a, int b)
+        {
+            int toplam = a + b;
+            return toplam;
+        }
+
+        static float Topla(float a, float b) // Değişken tipi farklı olduğu için aynı parametreleri verebilirim.
+        {
+            return a + b;
+        }
+
+        static decimal Topla(params decimal[] sayilar) // Girilen kadar tüm parametreyi toplamak için dizi yaptım.
+        {
+            decimal toplam = 0;
+            for (int i = 0;i < sayilar.Length; i++)
+            {
+                toplam += sayilar[i];
+            }
+            return toplam;
+        }
+
+        static int Cikar(int a, int b)
+        {
+            return (a - b);
+        }
+
+        static int Carp(int a, int b)
+        {
+            return (a * b);
+        }
+
+        static decimal Bol(int a, int b) // b = 0 olmamalı. Hata verir.
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            return a / (decimal)b;
+        } */
+    }
+}
